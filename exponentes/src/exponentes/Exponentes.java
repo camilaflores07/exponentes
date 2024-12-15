67256526560ccedb0c2cf0958ee80e3545bc6c85
Camila Flores <camila_maria@unitec.edu>
2024-12-14 19:48:05 -0600

    Add files via upload

---
+++ exponentes/src/exponentes/Exponentes.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exponentes;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 50494
+ */
+public class Exponentes {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double exponente,base,resultado;
+        
+        System.out.println("Favor ingresar una base");
+        base = exponente.nextDouble();
+        
+        System.out.println("Favor ingresar un exponente");
+        entrada = exponente.nextDouble();
+        
+        resultado = Math.pow(base, exponente);
+        
+        System.out.println("El resultado es:" +resultado);
+       
+    }
+    
+}
